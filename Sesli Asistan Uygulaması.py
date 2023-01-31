45b80c3d4141517b293342d2d02b4ba27ec53373
Şevval Ekinci <sevvalekinci6@gmail.com>
2023-01-31 22:03:44 +0300

    Create Sesli Asistan Uygulaması.py

---
+++ Sesli Asistan Uygulaması.py
@@ -0,0 +1,56 @@
+import speech_recognition as sr
+from datetime import datetime
+import webbrowser
+import time
+from gtts import gTTS
+from playsound import playsound
+import random
+import os
+
+r=sr.Recognizer()
+
+def record(ask=False):
+    with sr.Microphone() as source:
+        if ask:
+            speak(ask)
+        audio=r.listen(source)
+        voice=" "
+        try:
+            voice=r.recognize_google(audio,language="tr-TR")
+        except sr.UnknownValueError:
+            speak("Anlayamadım")
+        except sr.RequestError:
+            speak("Sistem Çalışmıyor")
+
+        return voice
+
+def response(voice):
+    if "nasılsın" in voice:
+        speak("İyiyim teşşekkürler sen nasılsın?")
+    if "saat kaç" in voice:
+        speak(datetime.now().strftime("%H:%M:%S"))
+    if "arama yap" in voice:
+        search=record("Ne aramak istiyorsun?")
+        url="https://google.com/search?q="+search
+        webbrowser.get().open(url)
+        speak(search+"  için bulduğum sonuçlar")
+    if "tamamdır" in voice:
+        speak("Görüşürüz")
+        exit()
+
+def speak(string):
+    tts=gTTS(string,lang="tr")
+    rand=random.randint(1,10000)
+    file=("audio-"+str(rand)+".mp3")
+    tts.save(file)
+    playsound(file)
+    os.remove(file)
+
+speak("Nasıl Yardımcı Olabilirim?")
+time.sleep(1)
+while 1:
+    voice=record()
+    print(voice)
+    response(voice)
+
+
